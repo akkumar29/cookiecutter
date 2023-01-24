c8e27a27c53648a6561917415ac66abf46681527
Aggarwal, Garima <gariaggarwal@deloitte.com>
2023-01-24 15:54:40 +0530

    Main app name render

--- {{cookiecutter.slug}}/src/main/java/com/{{cookiecutter.artifact}}/{{cookiecutter.project_name}}/{{cookiecutter.__main_app}}Application.java
+++ {{cookiecutter.slug}}/src/main/java/com/{{cookiecutter.artifact}}/{{cookiecutter.project_name}}/{{cookiecutter.__main_app}}Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class {{cookiecutter.main_app}}Application {
+public class {{cookiecutter.__main_app}}Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run({{cookiecutter.main_app}}Application.class, args);
+		SpringApplication.run({{cookiecutter.__main_app}}Application.class, args);
 	}
 
 }
